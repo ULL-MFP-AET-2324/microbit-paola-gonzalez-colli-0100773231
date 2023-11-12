803cdffd18aa915ac3a8a8d582c016928c125b17
Paola <148484314+Paolacolli@users.noreply.github.com>
2023-11-12 16:51:23 +0000

    contador de pasos

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
-input.onGesture(Gesture.Shake, function () {
-    basic.showNumber(randint(1, 6))
-    music.play(music.builtinPlayableSoundEffect(soundExpression.spring), music.PlaybackMode.UntilDone)
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(pasos)
 })
+input.onGesture(Gesture.Shake, function () {
+    pasos += 1
+    basic.showNumber(pasos)
+})
+let pasos = 0
+pasos = 0
